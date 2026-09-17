4c9d7b0bfa55f1c3065390505f768ea0e3d82c0f
Alex L <110128494+Pokiji@users.noreply.github.com>
2026-09-16 18:35:26 -0700

    -

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/misc/rpmIncra.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/misc/rpmIncra.java
@@ -4,26 +4,47 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opM
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 
 @TeleOp
 public class rpmIncra extends OpMode {
     private DcMotorEx s;
-    private  int velS = 1000;
+    private boolean toggle;
+
+    public GamepadEx p;
+    double targetRPM = 1000;
+    double ticksPerRev = 28;
     @Override
     public void init() {
         s =hardwareMap.get(DcMotorEx.class, "s");
+        toggle = false;
+        s.setDirection(DcMotor.Direction.REVERSE);
+        s.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        p = new GamepadEx(gamepad1);
     }
 
     @Override
     public void loop() {
-        if(gamepad1.b){
-            s.setVelocity(velS);
+
+        if(p.wasJustPressed(GamepadKeys.Button.A)){
+            if(!toggle){
+                s.setVelocity((targetRPM/60.0)*ticksPerRev);
+                toggle = true;
+            }else{
+                s.setVelocity(0);
+                toggle = false;
+            }
+
         }
-        if(gamepad1.a){
-            velS +=100;
-            s.setVelocity(velS);
+        if(p.wasJustPressed(GamepadKeys.Button.B)){
+            targetRPM +=100;
+            if(targetRPM >6000) targetRPM = 100;
+            s.setVelocity((targetRPM/60.0)*ticksPerRev);
         }
+        telemetry.addData("rpm:",(s.getVelocity()/60)*ticksPerRev);
+        telemetry.update();
     }
 }
